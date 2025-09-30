ff41b6aedb920836fa617321276423238e4bfbf2
fgpinterich <fgpinterich@mppr.mp.br>
2025-09-30 15:05:53 -0300

    added error messages

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -6,7 +6,18 @@ vim.o.relativenumber = true
 
 
 vim.o.ignorecase = true
-
 vim.o.cursorline = true
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+})
+
+vim.keymap.set("n", "ds", vim.diagnostic.open_float, { desc = "Show Diagnostic"})
 
